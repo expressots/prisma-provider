3fca4b88cb7b224ed127af5b78e45ac03296178e
juliano-soares <jlsoares@inf.ufsm.br>
2023-07-22 01:04:49 -0300

    fix: naming field elements fields

--- src/prisma/generators/relationship.helper.ts
+++ src/prisma/generators/relationship.helper.ts
@@ -31,12 +31,10 @@ async function createRelationships(
         return undefined;
     }
 
-    const relConstructor = {
-        relationStringTo: [] as string[], // store the relation string for the to entity
-        relationStringFrom: [] as string[], // store the relation of actual entity
-        newRowsTo: [] as string[], // store the new rows for the to entity
-        newRowsFrom: [] as string[], // store the new rows for the actual entity
-    };
+    const relationStringTo: string[] = []; // store the relation string for the to entity
+    const relationStringFrom: string[] = []; // store the relation of actual entity
+    const newRowsTo: string[] = []; // store the new rows for the to entity
+    const newRowsFrom: string[] = []; // store the new rows for the actual entity
 
     // get from class the property that is the relation
     const classRelation = fromClass.properties.find((property) => {
@@ -45,97 +43,121 @@ async function createRelationships(
         }
     });
 
-    // Create fields for relation (OneToOne, OneToManny, ManyToMany Explicit).
-    if (relationSettings.relation !== Relation.ManyToMany && relationSettings.refs) {
+    if (relationSettings.name) {
+        relationStringTo.push(`"${relationSettings.name}"`);
+        relationStringFrom.push(`"${relationSettings.name}"`);
+    }
+
+    if (relationSettings.refs) {
         // logic for create the foreign keys
-        const fks = relationSettings.fields ? relationSettings.fields.length : -1;
-        const pks = relationSettings.refs.length;
+        const fieldsLength = relationSettings.fields?.length;
+        const refsLength = relationSettings.refs.length;
 
-        const formattedFK: string[] = [];
-
-        switch (relationSettings.relation) {
-            case Relation.ManyToManyExplicit:
-                if (fks === pks && relationSettings.fields) {
-                    for (let i = 0; i < relationSettings.fields.length; i++) {
+        if (fieldsLength && fieldsLength < refsLength) {
+            printError(
+                "When adding elements in fields they must contain the same number of elements as refs",
+                `relation: ${relationSettings.name}`,
+            );
+            process.exit(1);
+        } else if (fieldsLength && fieldsLength > refsLength) {
+            printError(
+                "Number of elements in fields more than refs",
+                `relation: ${relationSettings.name}`,
+            );
+            process.exit(1);
+        }
+        // Create FK(fields) for relation (OneToOne, OneToManny, ManyToMany Explicit).
+        if (relationSettings.relation !== Relation.ManyToMany && relationSettings.refs) {
+            const formattedFK: string[] = [];
+            if (
+                fieldsLength === refsLength &&
+                relationSettings.fields &&
+                relationSettings.relation !== Relation.ManyToManyExplicit
+            ) {
+                for (let i = 0; i < relationSettings.fields.length; i++) {
+                    formattedFK.push(`${relationSettings.fields[i]}Id`);
+                }
+                relationStringTo.push(`fields: [${formattedFK.join(", ")}]`);
+            } else if (
+                !relationSettings.fields &&
+                relationSettings.relation !== Relation.ManyToManyExplicit
+            ) {
+                for (let i = 0; i < relationSettings.refs.length; i++) {
+                    if (i === 0) {
                         formattedFK.push(
-                            i === 0
-                                ? `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)}Id`
-                                : `${relationSettings.fields[i]}Id`,
+                            `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)}Id`,
                         );
-                    }
-                } else if (fks < pks) {
-                    for (let i = 0; i < relationSettings.refs.length; i++) {
-                        formattedFK.push(
-                            i === 0
-                                ? `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)}Id`
-                                : `${relationSettings.refs[i]}Id`,
-                        );
+                    } else {
+                        formattedFK.push(`${relationSettings.refs[i]}Id`);
                     }
                 }
-                break;
-            default:
-                if (fks === pks && relationSettings.fields) {
-                    for (let i = 0; i < relationSettings.fields.length; i++) {
+                relationStringTo.push(`fields: [${formattedFK.join(", ")}]`);
+            } else if (
+                fieldsLength === refsLength &&
+                relationSettings.fields &&
+                relationSettings.relation === Relation.ManyToManyExplicit
+            ) {
+                for (let i = 0; i < relationSettings.refs.length; i++) {
+                    formattedFK.push(`${relationSettings.refs[i]}Id`);
+                }
+                relationStringTo.push(`fields: [${formattedFK.join(", ")}]`);
+            } else if (
+                !relationSettings.fields &&
+                relationSettings.relation === Relation.ManyToManyExplicit
+            ) {
+                for (let i = 0; i < relationSettings.refs.length; i++) {
+                    if (i === 0) {
                         formattedFK.push(
-                            i === 0
-                                ? `${
-                                      relationSettings.model[0].toLowerCase() +
-                                      relationSettings.model.slice(1)
-                                  }Id`
-                                : `${relationSettings.fields[i]}Id`,
+                            `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)}Id`,
                         );
-                    }
-                } else if (fks < pks) {
-                    for (let i = 0; i < relationSettings.refs.length; i++) {
-                        formattedFK.push(
-                            i === 0
-                                ? `${
-                                      relationSettings.model[0].toLowerCase() +
-                                      relationSettings.model.slice(1)
-                                  }Id`
-                                : `${relationSettings.refs[i]}Id`,
-                        );
+                    } else {
+                        formattedFK.push(`${relationSettings.refs[i]}Id`);
                     }
                 }
-                break;
-        }
+                relationStringTo.push(`fields: [${formattedFK.join(", ")}]`);
+            }
 
-        if (formattedFK.length > 0) {
-            relConstructor.relationStringTo.push(`fields: [${formattedFK.join(", ")}]`);
-        }
+            if (classRelation?.pathDeclaration) {
+                const module = await import(path.resolve(filePath));
+                const entityClass = module[cls.name];
+                const fromClass = getDecorators(entityClass);
 
-        if (classRelation?.pathDeclaration) {
-            const module = await import(path.resolve(filePath));
-            const entityClass = module[cls.name];
-            const fromClass = getDecorators(entityClass);
-
-            for (let i = 0; i < relationSettings.refs.length && i < formattedFK.length; i++) {
-                for (const field of fromClass.fields) {
-                    if (field.name === relationSettings.refs[i]) {
-                        if (
-                            (relationSettings.relation === Relation.OneToOne ||
-                                relationSettings.relation === Relation.OneToMany) &&
-                            relationSettings.isRequired === true
-                        ) {
-                            relConstructor.newRowsTo.push(
-                                `${formattedFK[i]} ${field.type}? ${
-                                    formattedFK.length > 1 ? "" : " @unique"
-                                }`,
-                            );
-                        } else {
-                            relConstructor.newRowsTo.push(
-                                `${formattedFK[i]} ${field.type}${
-                                    formattedFK.length > 1 ? "" : " @unique"
-                                }`,
-                            );
+                if (formattedFK.length > 1) {
+                    for (let i = 0; i < formattedFK.length; i++) {
+                        for (const field of fromClass.fields) {
+                            if (field.name === relationSettings.refs[i]) {
+                                newRowsTo.push(`${formattedFK[i]} ${field.type}`);
+                            }
+                        }
+                    }
+                    newRowsFrom.push(`@@unique([${relationSettings.refs.join(", ")}])`);
+                    console.log(newRowsFrom);
+                    newRowsTo.push(`@@unique([${formattedFK.join(", ")}])`);
+                } else {
+                    for (let i = 0; i < formattedFK.length; i++) {
+                        for (const field of fromClass.fields) {
+                            if (field.name === relationSettings.refs[i]) {
+                                if (
+                                    (relationSettings.relation === Relation.OneToOne ||
+                                        relationSettings.relation === Relation.OneToMany) &&
+                                    relationSettings.isRequired === true
+                                ) {
+                                    newRowsTo.push(
+                                        `${formattedFK[i]} ${field.type}? ${
+                                            formattedFK.length > 1 ? "" : " @unique"
+                                        }`,
+                                    );
+                                } else {
+                                    newRowsTo.push(
+                                        `${formattedFK[i]} ${field.type}${
+                                            formattedFK.length > 1 ? "" : " @unique"
+                                        }`,
+                                    );
+                                }
+                            }
                         }
                     }
                 }
-            }
-
-            if (formattedFK.length > 1) {
-                relConstructor.newRowsFrom.push(`@@unique([${relationSettings.refs.join(", ")}])`);
-                relConstructor.newRowsTo.push(`@@unique([${formattedFK.join(", ")}])`);
             }
         }
     }
@@ -144,7 +166,7 @@ async function createRelationships(
     if (relationSettings.relation !== Relation.ManyToMany) {
         if (relationSettings.refs) {
             const formattedPK = relationSettings.refs.join(", ");
-            relConstructor.relationStringTo.push(`references: [${formattedPK}]`);
+            relationStringTo.push(`references: [${formattedPK}]`);
         } else {
             printError("You need to set refs for relation of type", `${relationSettings.relation}`);
             process.exit(1);
@@ -153,67 +175,92 @@ async function createRelationships(
 
     // Create onDelete for relation (OneToOne, OneToManny, ManyToMany Explicit)
     if (relationSettings.onDelete) {
-        relConstructor.relationStringTo.push(`onDelete: ${relationSettings.onDelete}`);
+        relationStringTo.push(`onDelete: ${relationSettings.onDelete}`);
     }
 
     // Create onUpdate for relation (OneToOne, OneToManny, ManyToMany Explicit)
     if (relationSettings.onUpdate) {
-        relConstructor.relationStringTo.push(`onUpdate: ${relationSettings.onUpdate}`);
+        relationStringTo.push(`onUpdate: ${relationSettings.onUpdate}`);
     }
 
     let relStringTo: string | undefined = undefined;
-    const relString = `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)} ${
-        fromClass.name
-    }`;
-
     if (relationSettings.relation !== Relation.ManyToMany) {
-        if (
-            (relationSettings.relation === Relation.OneToMany ||
-                relationSettings.relation === Relation.OneToOne) &&
-            relationSettings.isRequired === true
-        ) {
-            relStringTo = `${relString}?`;
+        if (relationSettings.isRequired === true) {
+            if (relationStringTo.length > 0) {
+                relStringTo = `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)} ${
+                    fromClass.name
+                }? @relation(${relationStringTo.join(", ")})`;
+            } else {
+                relStringTo = `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)} ${
+                    fromClass.name
+                }?`;
+            }
         } else {
-            relStringTo = relString;
+            if (relationStringTo.length > 0) {
+                relStringTo = `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)} ${
+                    fromClass.name
+                } @relation(${relationStringTo.join(", ")})`;
+            } else {
+                relStringTo = `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)} ${
+                    fromClass.name
+                }`;
+            }
         }
     } else {
-        relStringTo = `${relString}[]`;
-    }
-
-    if (relConstructor.relationStringTo.length > 0) {
-        relStringTo += ` @relation(${relConstructor.relationStringTo.join(", ")})`;
+        if (relationStringTo.length > 0) {
+            relStringTo = `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)} ${
+                fromClass.name
+            }[] @relation(${relationStringTo.join(", ")})`;
+        } else {
+            relStringTo = `${fromClass.name[0].toLowerCase() + fromClass.name.slice(1)} ${
+                fromClass.name
+            }[]`;
+        }
     }
 
     let relStringFrom: string | undefined = undefined;
-    const modelString = `${
-        relationSettings.model[0].toLowerCase() + relationSettings.model.slice(1)
-    } ${relationSettings.model}`;
-
-    if (relationSettings.relation === Relation.OneToMany) {
-        relStringFrom =
-            relConstructor.relationStringFrom.length > 0
-                ? `${modelString}[] @relation(${relConstructor.relationStringFrom.join(", ")})`
-                : `${modelString}[]`;
-    } else if (relationSettings.relation === Relation.ManyToManyExplicit && classRelation) {
-        const classRelName = classRelation?.name[0].toLowerCase() + classRelation?.name.slice(1);
-        relStringFrom =
-            relConstructor.relationStringFrom.length > 0
-                ? `${classRelName} ${
-                      relationSettings.model
-                  }[] @relation(${relConstructor.relationStringFrom.join(", ")})`
-                : `${classRelName} ${relationSettings.model}[]`;
+    if (relationSettings.relation !== Relation.ManyToMany) {
+        if (relationSettings.relation === Relation.OneToMany) {
+            if (relationStringFrom.length > 0) {
+                relStringFrom = `${
+                    relationSettings.model[0].toLowerCase() + relationSettings.model.slice(1)
+                } ${relationSettings.model}[] @relation(${relationStringFrom.join(", ")})`;
+            } else {
+                relStringFrom = `${
+                    relationSettings.model[0].toLowerCase() + relationSettings.model.slice(1)
+                } ${relationSettings.model}[]`;
+            }
+        } else if (relationSettings.relation === Relation.ManyToManyExplicit && classRelation) {
+            if (relationStringFrom.length > 0) {
+                relStringFrom = `${
+                    classRelation?.name[0].toLowerCase() + classRelation?.name.slice(1)
+                } ${relationSettings.model}[] @relation(${relationStringFrom.join(", ")})`;
+            } else {
+                relStringFrom = `${
+                    classRelation?.name[0].toLowerCase() + classRelation?.name.slice(1)
+                } ${relationSettings.model}[]`;
+            }
+        } else {
+            if (relationStringFrom.length > 0) {
+                relStringFrom = `${
+                    relationSettings.model[0].toLowerCase() + relationSettings.model.slice(1)
+                } ${relationSettings.model}? @relation(${relationStringFrom.join(", ")})`;
+            } else {
+                relStringFrom = `${
+                    relationSettings.model[0].toLowerCase() + relationSettings.model.slice(1)
+                } ${relationSettings.model}?`;
+            }
+        }
     } else {
-        relStringFrom =
-            relConstructor.relationStringFrom.length > 0
-                ? `${modelString}? @relation(${relConstructor.relationStringFrom.join(", ")})`
-                : `${modelString}?`;
-    }
-
-    if (relationSettings.relation === Relation.ManyToMany) {
-        relStringFrom =
-            relConstructor.relationStringFrom.length > 0
-                ? `${modelString} @relation(${relConstructor.relationStringFrom.join(", ")})`
-                : modelString;
+        if (relationStringFrom.length > 0) {
+            relStringFrom = `${
+                relationSettings.model[0].toLowerCase() + relationSettings.model.slice(1)
+            } ${relationSettings.model}[] @relation(${relationStringFrom.join(", ")})`;
+        } else {
+            relStringFrom = `${
+                relationSettings.model[0].toLowerCase() + relationSettings.model.slice(1)
+            } ${relationSettings.model}[]`;
+        }
     }
 
     const relashionships: Relationships = {
@@ -221,8 +268,8 @@ async function createRelationships(
         toEntity: relationSettings.model,
         relationStringTo: relStringTo,
         relationStringFrom: relStringFrom,
-        newRowsTo: relConstructor.newRowsTo,
-        newRowsFrom: relConstructor.newRowsFrom,
+        newRowsTo: newRowsTo,
+        newRowsFrom: newRowsFrom,
         relationType: relationSettings.relation,
         classRelation: classRelation,
     };
@@ -234,8 +281,8 @@ async function generatePrismaRelations(
     schemaPath: string,
     relations: Relationships[],
 ): Promise<void> {
+    let schemaContent = fs.readFileSync(schemaPath, "utf-8");
     relations.forEach(async (relation) => {
-        let schemaContent = fs.readFileSync(schemaPath, "utf-8");
         // Add new lines to the to entity
         const modelRegex = new RegExp(`(model\\s+${relation.toEntity}\\s+{[\\s\\S]*?})`, "g");
         const toEntity = schemaContent.match(modelRegex);
@@ -281,21 +328,16 @@ async function generatePrismaRelations(
                     schemaContent = schemaContent.replace(fromEntity[0], updatedSchemaContent);
                 }
             }
-
-            if (relation.newRowsFrom.length > 0) {
-                console.log(relation.newRowsFrom);
-                console.log(fromEntity[0]);
-                const newModel2 = fromEntity[0].replace(
-                    "}",
-                    `${relation.newRowsFrom.join("\n")}\n}`,
-                );
-
-                console.log(newModel2);
-                schemaContent = schemaContent.replace(fromEntity[0], newModel2);
-            }
         }
-        await fs.promises.writeFile(schemaPath, schemaContent);
+
+        const modelRegex3 = new RegExp(`(model\\s+${relation.fromEntity}\\s+{[\\s\\S]*?})`, "g");
+        const fromEntity1 = schemaContent.match(modelRegex3);
+        if (fromEntity1 && relation.newRowsFrom.length > 0) {
+            const newModel2 = fromEntity1[0].replace("}", `${relation.newRowsFrom.join("\n")}\n}`);
+            schemaContent = schemaContent.replace(fromEntity1[0], newModel2);
+        }
     });
+    await fs.promises.writeFile(schemaPath, schemaContent);
 }
 
 export { createRelationships, Relationships, generatePrismaRelations };
